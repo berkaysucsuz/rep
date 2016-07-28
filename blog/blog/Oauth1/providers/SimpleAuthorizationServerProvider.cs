a22ea872c09fe98c938c75309d8bc189fec1cb23
svgdmrl <demirelsevgi@outlook.com>
2016-07-28 13:45:05 +0300

    ilk
    whysoserious

---
+++ blog/blog/Oauth1/providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using blog.Models;
+using System.Linq;
+using System.Web.Http;
+using System;
+
+
+namespace blog.oauth1.providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public blogcontext db = new blogcontext();
+
+        // client erişimine izin verme
+        public override async System.Threading.Tasks.Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            
+            var query = from kullanici in db.kullanicilar
+                        where kullanici.kullaniciAd == context.UserName && kullanici.password == context.Password
+                        select kullanici;
+            int sonuc;
+            sonuc = query.Count();
+            //rol r = new rol();
+            //var query1 = from rol in db.rols where rol.rolId == k.kullaniciId select rol;
+            //r = query1.FirstOrDefault();
+
+            if (sonuc > 0)
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+                identity.AddClaim(new Claim("isim", context.UserName));
+                identity.AddClaim(new Claim("Roles", "admin"));
+                context.Validated(identity);
+                //kullanici k = query.First<kullanici>();
+                //token newtoken = new token();
+                //newtoken.tknaccess = k.kullaniciAd;
+                //newtoken.kullanici = k;
+                //newtoken.date = DateTime.Now;
+                //db.tokens.Add(newtoken);
+                //db.SaveChanges();
+        
+
+                    
+            }
+            else
+            {
+                context.SetError("invalid_grant", "kul veya şifre yanlış");
+            }
+        }
+    }
+}
---
+++ blog/blog/Oauth1/startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+using blog.oauth1;
+using blog.oauth1.providers;
+
+
+[assembly: OwinStartup(typeof(blog.oauth1.Startup))]
+namespace blog.oauth1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            ConfigureOAuth(appBuilder);
+
+            blog.WebApiConfig.Register(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+
+        private void ConfigureOAuth(IAppBuilder appBuilder)
+        {
+            
+            OAuthAuthorizationServerOptions oAuthAuthorizationServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new Microsoft.Owin.PathString("api/token"), // token path
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                AllowInsecureHttp = true,
+                Provider = new SimpleAuthorizationServerProvider()
+
+            };
+
+            appBuilder.UseOAuthAuthorizationServer(oAuthAuthorizationServerOptions);//token üretimini gerçekleştrme
+
+            appBuilder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
